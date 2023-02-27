99f85e7b3b95b3dfe6a36b1c64556ac976b929d4
g218suna <g520218m@mails.cc.ehime-u.ac.jp>
2023-02-28 00:48:48 +0900

    サインアウト機能の作成完了

--- src/feature/auth/provider/AuthProvider.tsx
+++ src/feature/auth/provider/AuthProvider.tsx
@@ -1,19 +1,19 @@
 import {
-  createContext,
-  ReactNode,
-  useContext,
-  useEffect,
-  useState,
+    createContext,
+    ReactNode,
+    useContext,
+    useEffect,
+    useState,
 } from 'react'
 import type { User } from '@firebase/auth'
 import { getAuth, onAuthStateChanged } from '@firebase/auth'
 
 export type GlobalAuthState = {
-  user: User | null | undefined
+    user: User | null | undefined
 }
 
 const initialState: GlobalAuthState = {
-  user: undefined,
+    user: undefined,
 }
 
 const AuthContext = createContext<GlobalAuthState>(initialState)
@@ -21,24 +21,24 @@ const AuthContext = createContext<GlobalAuthState>(initialState)
 type Props = { children: ReactNode }
 
 export const AuthProvider = ({ children }: Props) => {
-  const [user, setUser] = useState<GlobalAuthState>(initialState)
+    const [user, setUser] = useState<GlobalAuthState>(initialState)
 
-  useEffect(() => {
-    try {
-      const auth = getAuth()
-      return onAuthStateChanged(auth, (user) => {
-        setUser({
-          user,
-        })
-      })
-    } catch (error) {
-      setUser(initialState)
-      throw error
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
+    useEffect(() => {
+        try {
+            const auth = getAuth()
+            return onAuthStateChanged(auth, (user) => {
+                setUser({
+                    user,
+                })
+            })
+        } catch (error) {
+            setUser(initialState)
+            throw error
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
 
-  return <AuthContext.Provider value={user}>{children}</AuthContext.Provider>
+    return <AuthContext.Provider value={user}>{children}</AuthContext.Provider>
 }
 
 export const useAuthContext = () => useContext(AuthContext)
